d2afd9ef2b3499222cba507b2dc70a5a569e4048
Renatinho <renatoboss001@gmail.com>
2024-04-24 19:50:26 -0300

    game over(victory or draw) checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,87 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 
-const Box = ({XTurn, setXTurn}) => {
-
-  const [current, setCurrent] = useState('');
-
+const Box = ({value, index, running, setBoard, XTurn, setXTurn, selected = false}) => {
+  
+  
   const handleClick = () => {
-    console.log('ain que delicia');
-    if (current != '') return;
-    XTurn ? setCurrent('X') : setCurrent('O');
-    setXTurn(!XTurn);
+    
+    if(value != '' || !running) return;
+
+    setBoard((prev) => {
+
+      const copy = [...prev];
+      copy[index] = {value: XTurn ? 'X' : 'O', index: index};
+
+      return copy;
+
+    })
+
+    setXTurn(!XTurn)
+
   }
-
+  
   return(
-    <div onClick={handleClick} className="box">
-      {current}
-    </div>
+    <div style={{backgroundColor: selected ? 'yellowgreen' : undefined}} onClick={handleClick} className='box'>{value}</div>
   )
-
 
 }
 
 function App() {
 
-  const [XTurn, setXTurn] = useState(true);  
+  const [XTurn, setXTurn] = useState(true);
+  const [running, setRunning] = useState(true);
+  const [board, setBoard] = useState([
+
+    {value: '', index: 0, selected: false},
+    {value: '', index: 1, selected: false},
+    {value: '', index: 2, selected: false},
+    {value: '', index: 3, selected: false},
+    {value: '', index: 4, selected: false},
+    {value: '', index: 5, selected: false},
+    {value: '', index: 6, selected: false},
+    {value: '', index: 7, selected: false},
+    {value: '', index: 8, selected: false}
+  ]);
+
+  const winningCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+
+    [0, 4, 8], [2, 4, 6]
+  ];
+
+  if(running) {
+    let win = false;
+    const value = XTurn ? 'O' : 'X';
+    winningCombinations.forEach((combination) => {
+      if(board[combination[0]].value == value && board[combination[1]].value == value && board[combination[2]].value == value){
+        const copy = [...board];
+        copy[combination[0]] = {value: value, index: copy[combination[0]].index, selected: true};
+        copy[combination[1]] = {value: value, index: copy[combination[1]].index, selected: true};
+        copy[combination[2]] = {value: value, index: copy[combination[2]].index, selected: true};
+        setRunning(false);
+        setBoard(copy);
+        win = true;
+      }
+    })
+
+    let draw = false;
+    for(let i = 0; i < board.length; i++) {
+      if(board[i].value == '') {break;}
+      if(i == board.length - 1) {draw = true;}
+    }
+  
+    if(draw && !win) {
+      console.log('empate');
+      setRunning(false);
+    }
+  }
+
+
 
   return (
-    
     <div className="container">
       <h3>Turn for</h3>
       <div className="turn-container">
@@ -35,15 +90,11 @@ function App() {
       </div>
 
       <div className="game-container">
-        <Box XTurn={XTurn} setXTurn={setXTurn}/>
-        <Box XTurn={XTurn} setXTurn={setXTurn}/>
-        <Box XTurn={XTurn} setXTurn={setXTurn}/>
-        <Box XTurn={XTurn} setXTurn={setXTurn}/>
-        <Box XTurn={XTurn} setXTurn={setXTurn}/>
-        <Box XTurn={XTurn} setXTurn={setXTurn}/>
-        <Box XTurn={XTurn} setXTurn={setXTurn}/>
-        <Box XTurn={XTurn} setXTurn={setXTurn}/>
-        <Box XTurn={XTurn} setXTurn={setXTurn}/>
+        {board.map((item) => {
+          return(
+            <Box selected={item.selected} running={running} key={item.index} XTurn={XTurn} setXTurn={setXTurn} value={item.value} index={item.index} setBoard={setBoard}/>
+          )
+        })}
       </div>
     </div>
 
